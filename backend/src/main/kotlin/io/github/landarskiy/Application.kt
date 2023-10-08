1e5e556356b4ce8ca6245d57654472bd36c36988
Yauhen Landarski <landarskiy@gmail.com>
2023-10-09 01:59:47 +0200

    Update readme

--- backend/src/main/kotlin/io/github/landarskiy/Application.kt
+++ backend/src/main/kotlin/io/github/landarskiy/Application.kt
@@ -1,15 +1,18 @@
 package io.github.landarskiy
 
-import io.github.landarskiy.plugins.*
+import io.github.landarskiy.plugins.configureClient
+import io.github.landarskiy.plugins.configureCors
+import io.github.landarskiy.plugins.configureRouting
+import io.github.landarskiy.plugins.configureSerialization
 import io.github.landarskiy.repository.configureRepository
 import io.ktor.server.application.*
-import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
-fun main(args: Array<String>) : Unit = EngineMain.main(args)
+fun main(args: Array<String>): Unit = EngineMain.main(args)
 
 // Called from engine
 fun Application.module() {
+    configureClient()
     configureRepository()
     configureRouting()
     configureCors()
