02f62305f68d02805496c74af7e76283df47967f
Yauhen Landarski <landarskiy@gmail.com>
2023-10-10 17:25:12 +0200

    Fix config

--- webapp/config.js
+++ webapp/config.js
@@ -2,6 +2,5 @@
  * The app configuration
  */
 const configuration = {
-    serverUrl: "http://0.0.0.0:8080"
-    //serverUrl: "https://tgminiapp-65728c571d53.herokuapp.com"
+    serverUrl: "https://tgminiapp-65728c571d53.herokuapp.com"
 }
